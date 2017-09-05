cac45b59cc30f6e5e56946ba3aca731b724bf1a4
Felix E. C. Klemke <me@felixklemke.com>
2017-09-05 23:09:11 +0200

    generic test run method added / refactoring

--- testWRK.py
+++ testWRK.py
@@ -3,8 +3,8 @@ import time
 import re
 import toolBox
 
-timeWaitForEphePorts = 25
-testParameterWrk =  "wrk -t2 -d60s -c10 --latency --timeout 100s"
+timeWaitForEphePorts = 15
+testParameterWrk =  "wrk -t2 -d20s -c10 --latency --timeout 100s"
 httpAdd = " http://169.254.90.222"
 directory = "./LogFiles/"
 wrk2RatePercentage = 0.5
@@ -12,9 +12,10 @@ useCaseRequestSecValue = 100
 
 
 ######################################################################
-def logTime(testcase, title, path):
-    with open("./" + testcase + ".txt", "a") as f:
+def logTime(testcase, title, urlPath):
+    with open(directory + testcase + ".txt", "a") as f:
         f.write("\n" + time.strftime("%Y/%m/%d %H:%M:%S") + " " + testcase + " " + title + "\n" + path + "\n")
+
 def iterateDirectoryForWrk2Test():
     print "Iterating directory to process wrk2 tests"
     time.sleep(10)
@@ -55,116 +56,83 @@ def jsonTest():
     time.sleep(timeWaitForEphePorts)
     testcase = directory + "JsonPerfectClientWrkV1"
     title = "Perfect JSON benchmarking / Business case"
-    print title
     path = testParameterWrk + httpAdd + ":8081/jsonPerfect"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "JsonVaporClientWrkV1"
     title = "Vapor JSON benchmarking / Business case"
-    print title
     path = testParameterWrk + httpAdd + ":8080/jsonVapor"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "JsonKituraClientWrkV1"
     title = "Kitura JSON benchmarking / Business case"
-    print title
     path = testParameterWrk + httpAdd + ":8282/jsonKitura"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 def jsonSmallTest():
     os.system('echo "WRK test started"')
     testcase = directory + "JsonPerfectClientWrkV2"
     title = "Perfect JSON benchmarking"
-    print title
     path = testParameterWrk + httpAdd + ":8081/jsonShortPerfect"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "JsonVaporClientWrkV2"
     title = "Vapor JSON benchmarking"
-    print title
     path = testParameterWrk + httpAdd +":8080/jsonShortVapor"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "JsonKituraClientWrkV2"
     title = "Kitura JSON benchmarking"
-    print title
     path = testParameterWrk + httpAdd + ":8282/jsonShortKitura"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 def htmlTest():
     testcase = directory + "HtmlPerfectClientWrkV1"
     title = "Perfect HTML benchmarking"
-    print title
     path = testParameterWrk+httpAdd +":8181/jsonShortPerfect"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase,title,path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "HtmlVaporClientWrkV1"
     title = "Vapor HTML benchmarking"
-    print title
     path = testParameterWrk+httpAdd +":8080/jsonShortVapor"
-    os.system(path+" > " + testcase + ".txt")
-    logTime(testcase,title,path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "HtmlKituraClientWrkV1"
     title = "Kitura HTML benchmarking"
-    print title
     path = testParameterWrk+httpAdd +":8282/jsonShortKitura"
-    os.system(path+" > " + testcase+ ".txt")
-    logTime(testcase,title,path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 def plaintextTest():
     testcase = directory + "PlaintextPerfectClientWrkV1"
     title = "Perfect plaintext benchmarking"
-    print title
     path = testParameterWrk + httpAdd + ":8081/jsonShortPerfect"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title,path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "PlaintextVaporClientWrkV1"
     title = "Vapor plaintext benchmarking"
-    print title
     path = testParameterWrk + httpAdd +":8080/jsonShortVapor"
-    os.system(path + " > " + testcase+ ".txt")
-    logTime(testcase, title,path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 
     testcase = directory + "PlaintextKituraClientWrkV1"
     title = "Kitura plaintext benchmarking"
-    print title
     path = testParameterWrk + httpAdd + ":8282/jsonShortKitura"
-    os.system(path + " > " + testcase+ ".txt")
+    genericTestRun(testcase, title, path)
+
+def genericTestRun(testcase, title, path):
+    print title
+    os.system(path + " > " + directory + testcase + ".txt")
     logTime(testcase, title, path)
     time.sleep(timeWaitForEphePorts)
 ######################################################################
 
 
 def testMethod():
-    testcase = directory + "JsonPerfectClientWrkV1"
+    testcase = "JsonPerfectClientWrkV1"
     title = "Perfect JSON benchmarking / Business case"
-    print title
     path = testParameterWrk + httpAdd + ":8081/jsonPerfect"
-    os.system(path + " > " + testcase + ".txt")
-    logTime(testcase, title, path)
-    time.sleep(timeWaitForEphePorts)
+    genericTestRun(testcase, title, path)
 ######################################################################
 
-jsonTest()
-jsonSmallTest()
-htmlTest()
-plaintextTest()
-# testMethod()
+# jsonTest()
+# jsonSmallTest()
+# htmlTest()
+# plaintextTest()
+testMethod()
 iterateDirectoryForWrk2Test()
